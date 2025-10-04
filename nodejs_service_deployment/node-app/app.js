04752bd8edae0c2fcbef0dc351f232369c1b5c34
alim7007 <olimtoni7007@gmail.com>
2025-10-04 04:15:37 +0300

    fix(nodejs): set localhost to all interface

--- nodejs_service_deployment/node-app/app.js
+++ nodejs_service_deployment/node-app/app.js
@@ -1,12 +1,12 @@
-// app.js
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const HOST = "0.0.0.0"; // Listen on all interfaces
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
